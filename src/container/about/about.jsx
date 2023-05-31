30e1eac4f4531c352119967a080edce689a74436
Sandeep Yadav <sandyadav434@gmail.com>
2023-05-31 17:55:41 +0530

    Backend & File_Folder

---
+++ src/container/about/about.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './about.scss'
+
+const About = () => {
+  return (
+    <div>about</div>
+  )
+}
+
+export default About
---
+++ src/container/skills/skills.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './skills.scss'
+
+const Skills = () => {
+  return (
+    <div>skills</div>
+  )
+}
+
+export default Skills
